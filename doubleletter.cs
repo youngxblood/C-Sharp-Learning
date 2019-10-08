a3a1cf3973e4c594afb8aa07ee67473551174040
Mark Steffensen <crazyeight68@gmail.com>
2019-10-07 18:07:02 -0700

    Program.cs now holds code for IndexOfCapitals

---
+++ doubleletter.cs
@@ -0,0 +1,20 @@
+// public class Program
+// {
+//     public static bool DoubleLetters(string word)
+//     {
+// 			char[] charArr = word.ToCharArray();
+
+
+// 			// for (int i = 0; i < 3; i++)
+// 			for (int i = 0; i < charArr.Length -1; i++)
+// 			{
+// 				if (charArr[i] == charArr[i + 1])
+// 				{
+// 					return true;
+// 				} else
+// 				{
+// 					return false;
+// 				}
+// 			}
+//     }
+// }
