4cd20a187ac34b828cf12b84d4996d7ad2bee099
youngxblood <crazyeight68@gmail.com>
2019-10-12 12:32:58 -0700

    Restructuring directories

--- Program.cs
+++ Program.cs
@@ -4,11 +4,6 @@ namespace myapp
 {
     class Program
     {
-        public static string ConcatName(string firstName, string lastName)
-        {
-            string resultStr = lastName + ", " + firstName;
-            return resultStr;
-        }
         // Main program here
         static void Main(string[] args)
         {
